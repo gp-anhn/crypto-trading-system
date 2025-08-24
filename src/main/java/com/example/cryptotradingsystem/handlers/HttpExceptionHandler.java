aff1bd0711547a80a11b76fe0d232e65d92f3ca5
Huynh Ngoc An <huynhngocan1720@gmail.com>
2025-08-24 16:17:56 +0700

    feat: add exception handler

---
+++ src/main/java/com/example/cryptotradingsystem/handlers/HttpExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.cryptotradingsystem.handlers;
+
+import com.google.gson.JsonObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class HttpExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        error.put("status", HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
